f7a627b33eb8e41f53fcc430189c6daac06005ed
Sameer Purohit <149887567+Sameer714@users.noreply.github.com>
2025-05-07 19:30:26 +0530

    event and user controllers

--- src/main/java/com/example/EventManagementPlatform/controller/EventController.java
+++ src/main/java/com/example/EventManagementPlatform/controller/EventController.java
@@ -1,8 +1,31 @@
 package com.example.EventManagementPlatform.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.example.EventManagementPlatform.exception.DuplicateUsernameException;
+import com.example.EventManagementPlatform.model.Event;
+import com.example.EventManagementPlatform.service.EventService;
+
+@RestController
 public class EventController {
-
+	
+	@Autowired
+	EventService eventService;
+	
+	@PostMapping
+	public ResponseEntity<Object> saveEvent (@RequestBody Event event) {
+		try {
+			eventService.saveEvent(event);
+		}
+		catch(DuplicateUsernameException e) {
+			return ResponseEntity.status(HttpStatus.OK)
+					.body("{\"message\": \"" + e.getMessage() + "\" , \"Success\": \"" + "false" + "\"}");
+		}
+		return null;
+	}
 }
--- src/main/java/com/example/EventManagementPlatform/controller/UserController.java
+++ src/main/java/com/example/EventManagementPlatform/controller/UserController.java
@@ -1,8 +1,46 @@
 package com.example.EventManagementPlatform.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.EventManagementPlatform.exception.DuplicateUsernameException;
+import com.example.EventManagementPlatform.model.User;
+import com.example.EventManagementPlatform.service.UserService;
 
 @RestController
 public class UserController {
-
+	@Autowired
+	UserService userService;
+	
+	@PostMapping
+	public ResponseEntity<Object> saveAdmin(@RequestBody User user) {
+		try {
+			User saveAdmin = userService.saveAdmin(user);
+			if (saveAdmin != null) {
+				if (saveAdmin.getRole().equalsIgnoreCase("ROLE_ADMIN")) {
+					return ResponseEntity.status(HttpStatus.OK)
+							.body("{\"message\": \"" + "User Saved Successfully , Wait for Admin Approval!"
+									+ "\" ,  \"Success\": \"" + "true" + "\"}");
+				}
+				else if (saveAdmin.getRole().equalsIgnoreCase("ROLE_USER")) {
+					return ResponseEntity.status(HttpStatus.OK).body(
+							"{\"message\": \"" + "User Saved Successfully" + "\" ,  \"Success\": \"" + "true" + "\"}");
+				}
+				else if (saveAdmin.getRole().equalsIgnoreCase("ROLE_ORGANIZER")) {
+					return ResponseEntity.status(HttpStatus.OK)
+							.body("{\"message\": \"" + "User Saved Successfully , Wait for Admin Approval!"
+									+ "\" ,  \"Success\": \"" + "true" + "\"}");
+				}
+			}
+		}		
+		catch (DuplicateUsernameException e) {
+			return ResponseEntity.status(HttpStatus.OK)
+					.body("{\"message\": \"" + e.getMessage() + "\" , \"Success\": \"" + "false" + "\"}");
+		}
+		return null;
+	}
 }
